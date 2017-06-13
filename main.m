ca4fc667e4af0a213bae1b0b2222a8e3e8f01ba6
Tri Fennia Lesmana <tfennia@gmail.com>
2017-06-13 20:27:42 +0700

    PSO adding function

--- main.m
+++ main.m
@@ -38,30 +38,55 @@ for i=1:nClasses
 end
 
 % Particle Swarm Optimization (PSO) process
-nParticles = 20;
+nParticles = 2; % ganti-ganti
 nFeatures = 17;
 nBits = size(decToBin(size(trainingData, 1)), 2); %bin2 = de2bi(nSamples);
 
 % Population Initialization: [FeatureMask HiddenNode]
-population = rand(nParticles, nFeatures+nBits) > 0.5;
+population = rand(nParticles, nFeatures+nBits) > 0.8;
 fitnessValue = zeros(nParticles, 1);
 velocity = zeros(nParticles, 1);
-pBest = zeros(nParticles, nFeatures+nBits); % max fitness function 
-gBest = zeros(1, nFeatures+nBits); % max fitness function all particle all iteration
+pBest_particle = zeros(nParticles, nFeatures+nBits); % max fitness function
+pBest_fitness = repmat(-1000000, nParticles, 1);
+gBest_particle = zeros(1, nFeatures+nBits); % max fitness function all particle all iteration
+gBest_fitness = -1000000;
 %featureMask = [1 1 1 1 0  1 1 0 0 1  1 1 1 0 0  0 0];
 %featureMask = [1 1 1 1 1  1 1 1 1 1  1 1 1 1 1  1 1];
 for i=1:nParticles
+    fprintf('ELM training particle %d... number of hidden node: %d\n', i, binToDec(population(i, nFeatures+1:end)));
     % TRAINING
     maskedTrainingFeature = featureMasking(trainingData, population(i, 1:nFeatures));% prepare the feature data (masking)
     trainingTarget = full(ind2vec(trainingData(:,end)'))';% prepare the target data (transformation from 4 into [0 0 0 1 0 0])
     elmModel = trainELM(binToDec(population(i, nFeatures+1:end)), maskedTrainingFeature, trainingTarget);
-
+    
     % TESTING
     maskedTestingFeature = featureMasking(testingData, population(i, 1:nFeatures));% prepare the feature data (masking)
     testingTarget = full(ind2vec(testingData(:,end)'))';% prepare the target data (transformation from 4 into [0 0 0 1 0 0])
     elmModel = testELM(elmModel, maskedTestingFeature, testingTarget);
     fitnessValue(i, 1) = fitness(0.95, 0.05, elmModel.testingAccuracy, population(i, 1:nFeatures));
     
-    %disp('hai')
+    disp('hai')
 end
+
+% pBest Update
+for i=1:nParticles
+    if fitnessValue(i, 1) > pBest_fitness(i, 1)
+        pBest_fitness(i, 1) = fitnessValue(i, 1);
+        pBest_particle(i, :) = population(i, :);
+    end
+end
+
+% gBest Update
+if max(fitnessValue) > gBest_fitness
+    found = find(fitnessValue == max(fitnessValue));
+    found = found(1);
+    gBest_fitness = max(fitnessValue);
+    gBest_particle = population(found, :);    
+end
+
+% Update velocity
+W = 0.6;
+c1 = 1.2;
+c2 = 1.2;
+
 toc;
