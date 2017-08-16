4e5c6133e602ba02054290241270ba289f64d494
Tri Fennia Lesmana <fennia@gmail.com>
2017-08-16 23:14:55 +0700

    ELM only

--- ELMFullFeatureExperiment.m
+++ ELMFullFeatureExperiment.m
@@ -5,8 +5,8 @@ fileNames = {'slp01a' 'slp01b' 'slp02a' 'slp02b' 'slp03' 'slp04' ...
 classNum = [2 3 4 6];
 MAX_EXPERIMENT = 25;
 MAX_ITERATION = 100;
-
-mkdir('ELMFullFeatureResults');
+rootFolder = 'ELM_result';
+mkdir(rootFolder);
 % load features and targets
 hrv = loadmatobject('features/hrv_features_norm.mat', 1);
 nFeatures = size(hrv, 2);
@@ -15,6 +15,8 @@ target = loadmatobject('features/target.mat', 1);
 nRecSamples = loadmatobject('nRecSamples.mat', 1);
 
 for iFile=1:length(fileNames)
+    eachFileFolder = sprintf('%s/ELM_%s_result/', rootFolder, fileNames{iFile});
+    mkdir(eachFileFolder);
     for iClass=1:length(classNum)
         nClasses = classNum(iClass); % nClasses = total output class
         %targetTemp = target(:, nClasses); % prepare target
@@ -36,9 +38,10 @@ for iFile=1:length(fileNames)
         
         minTotalHiddenNodes = nFeatures;
         maxTotalHiddenNodes = size(trainingData, 1);
-        allExperiments = zeros(MAX_EXPERIMENT, 3);
+        allExperiments = zeros(MAX_EXPERIMENT, 4);
         
         for iExp=1:MAX_EXPERIMENT
+            tic;
             fprintf('Building iFile = %d/%d, iClass = %d/%d, iExp = %d/%d\n', iFile, length(fileNames), iClass, length(classNum), iExp, MAX_EXPERIMENT);
             oneExperiment = zeros(MAX_ITERATION, 3);
             for iIter=1:MAX_ITERATION
@@ -56,16 +59,32 @@ for iFile=1:length(fileNames)
                 oneExperiment(iIter, 2) = testAcc;
                 oneExperiment(iIter, 3) = nHiddenNode;
             end
-            bestOfExperiment = oneExperiment(oneExperiment(:, 2) == max(oneExperiment(:, 2)), :);
-            if length(bestOfExperiment) > 1
-                bestOfExperiment = bestOfExperiment(bestOfExperiment(:, 1) == max(bestOfExperiment(:, 1)), :);
-                if length(bestOfExperiment) > 1
-                    bestOfExperiment = bestOfExperiment(bestOfExperiment(:, 3) == min(bestOfExperiment(:, 3)), :);
+            bestOfOneExperiment = oneExperiment(oneExperiment(:, 2) == max(oneExperiment(:, 2)), :);
+            if length(bestOfOneExperiment) > 1
+                bestOfOneExperiment = bestOfOneExperiment(bestOfOneExperiment(:, 1) == max(bestOfOneExperiment(:, 1)), :);
+                if length(bestOfOneExperiment) > 1
+                    bestOfOneExperiment = bestOfOneExperiment(bestOfOneExperiment(:, 3) == min(bestOfOneExperiment(:, 3)), :);
                 end
             end
-            disp('hai');
+            bestOfOneExperiment = bestOfOneExperiment(1, :);
+            xlswrite(sprintf('%s/ELM_%s_experiment_%d.xlsx', eachFileFolder, fileNames{iFile}, iExp), {'TrainAcc', 'TestAcc', 'HiddenNode'}, sprintf('%d classes', nClasses));
+            xlswrite(sprintf('%s/ELM_%s_experiment_%d.xlsx', eachFileFolder, fileNames{iFile}, iExp), oneExperiment, sprintf('%d classes', nClasses), 'A2')
+            timeNeeded = toc;
+            allExperiments(iExp, :) = [bestOfOneExperiment timeNeeded];
         end
         
-        save(sprintf('%s/%s_%s_%dclasses_raw_result.mat', path, method, fileNames{iFile}, nClasses), 'ExperimentResult', '-v7.3');
+        xlswrite(sprintf('%s/ELM_%s_experiment_result.xlsx', eachFileFolder, fileNames{iFile}), {'TrainAcc', 'TestAcc', 'HiddenNode', 'Time (sec)'}, sprintf('%d classes', nClasses));
+        xlswrite(sprintf('%s/ELM_%s_experiment_result.xlsx', eachFileFolder, fileNames{iFile}), allExperiments, sprintf('%d classes', nClasses), 'A2')
+        bestOfAllExperiments = allExperiments(allExperiments(:, 2) == max(allExperiments(:, 2)), :);
+        if length(bestOfAllExperiments) > 1
+            bestOfAllExperiments = bestOfAllExperiments(bestOfAllExperiments(:, 1) == max(bestOfAllExperiments(:, 1)), :);
+            if length(bestOfAllExperiments) > 1
+                bestOfAllExperiments = bestOfAllExperiments(bestOfAllExperiments(:, 3) == min(bestOfAllExperiments(:, 3)), :);
+            end
+        end
+        bestOfAllExperiments = bestOfAllExperiments(1, :);
+        xlswrite(sprintf('%s/ELM_%s_result.xlsx', rootFolder, fileNames{iFile}), {'File Name', 'TrainAcc', 'TestAcc', 'HiddenNode', 'Time (sec)'}, sprintf('%d classes', nClasses));
+        xlswrite(sprintf('%s/ELM_%s_result.xlsx', rootFolder, fileNames{iFile}), fileNames(iFile), sprintf('%d classes', nClasses), sprintf('A%d', iFile+1))
+        xlswrite(sprintf('%s/ELM_%s_result.xlsx', rootFolder, fileNames{iFile}), bestOfAllExperiments, sprintf('%d classes', nClasses), sprintf('B%d', iFile+1))
     end
 end
